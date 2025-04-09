e2daf5c7c60edcf0661cbbe5dbe9c3a87296c774
Dan <voda.daniel98@yahoo.com>
2025-04-09 19:35:14 +0300

    Created /validate endpoint with validation logic

--- auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
+++ auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
@@ -1,5 +1,7 @@
 package com.pm.authservice.util;
 
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.slf4j.Logger;
@@ -7,8 +9,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.security.SignatureException;
 import java.util.Base64;
 import java.util.Date;
 
@@ -32,5 +36,15 @@ public class JwtUtil {
                 .expiration(new Date(System.currentTimeMillis()+1000*60*60*10)) // 10 hours
                 .signWith(secretKey)
                 .compact();
+    }
+
+    public void validateToken(String token) {
+        try {
+            Jwts.parser().verifyWith((SecretKey) secretKey)
+                    .build()
+                    .parseSignedClaims(token);
+        } catch (JwtException e) {
+            throw new JwtException("Invalid token");
+        }
     }
 }
